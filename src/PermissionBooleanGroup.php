ba422ce1a4d7c8c470037a036dfe6fa08ceff01f
Süleyman ÖZEV <info@suleymanozev.com>
2022-03-13 02:19:30 +0300

    Fix RoleBooleanGroup and PermissionBooleanGroup

--- src/PermissionBooleanGroup.php
+++ src/PermissionBooleanGroup.php
@@ -26,9 +26,9 @@ class PermissionBooleanGroup extends BooleanGroup
 
         $permissionClass = app(PermissionRegistrar::class)->getPermissionClass();
 
-        $options = $permissionClass::get()->pluck($labelAttribute ?? 'name', 'name')->filter(function ($permission) {
-            return Auth::user()->can('view', PermissionModel::where('name', $permission)->first());
-        })->toArray();
+        $options = $permissionClass::all()->filter(function ($permission) {
+            return Auth::user()->can('view', $permission);
+        })->pluck($labelAttribute ?? 'name', 'name');
 
         $this->options($options);
     }
