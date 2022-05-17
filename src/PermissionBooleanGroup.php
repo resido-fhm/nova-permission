150b3541f373d3bbf7d8a215b719674e408c2e9a
Süleyman ÖZEV <suleymanozev@protonmail.com>
2022-05-17 06:27:42 +0300

    Fixes #15

--- src/PermissionBooleanGroup.php
+++ src/PermissionBooleanGroup.php
@@ -17,8 +17,8 @@ class PermissionBooleanGroup extends BooleanGroup
         parent::__construct(
             $name,
             $attribute,
-            $resolveCallback ?? static function (Collection $permissions) {
-                return $permissions->mapWithKeys(function (PermissionModel $permission) {
+            $resolveCallback ?? static function (?Collection $permissions) {
+                return ($permissions ?? collect())->mapWithKeys(function (PermissionModel $permission) {
                     return [$permission->name => true];
                 });
             }
